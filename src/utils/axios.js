4e2e5a1009bbab5b49c320b235f9493ce7aa6e8b
SanjeevRaj371 <ridzysanju@gmail.com>
2026-01-07 21:18:02 +0530

    day-1.1

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "https://basic-e-commerses-website-backend.onrender.com/",
   headers: {
     'Content-Type': 'application/json',
   },
